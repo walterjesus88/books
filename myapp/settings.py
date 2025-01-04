1c2d904336ea18569c5c34b485cc031986e6fbfa
Walter <wjesus88@gmail.com>
2025-01-04 00:02:05 -0500

    nginx conf3

--- myapp/settings.py
+++ myapp/settings.py
@@ -135,9 +135,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+#STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # En caso de que estés usando archivos multimedia
