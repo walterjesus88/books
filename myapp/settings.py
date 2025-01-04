9c2a40df21ece874a18ec18fc169add625be31d8
Walter <wjesus88@gmail.com>
2025-01-04 00:04:32 -0500

    nginx conf4

--- myapp/settings.py
+++ myapp/settings.py
@@ -21,9 +21,9 @@ SIMPLE_JWT = {
     'SIGNING_KEY': SECRET_KEY,
 }
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 
 INSTALLED_APPS = [
